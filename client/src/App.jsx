87440a25ca84946f90a8e6b016f5d2f68479a328
Your Name <you@example.com>
2024-03-21 17:06:30 +0100

    creat pages and outes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Members from './pages/Members'
+import News from './pages/News'
+import Projects from './pages/Projects'
+import Publications from './pages/Publications'
+import SignUp from './pages/SignUp'
+import LogIn from './pages/LogIn'
+import Research from './pages/Research'
+
 
 export default function App () {
   return (
-    <>
-    <h1>app</h1>
-    </>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path ="/" element= {<Home/>}/>
+      <Route path ="/Contact" element= {<Contact/>}/>
+      <Route path ="/About" element= {<About/>}/>
+      <Route path ="/Dashboard" element= {<Dashboard/>}/>
+      <Route path ="/Members" element= {<Members/>}/>
+      <Route path ="/News" element= {<News/>}/>
+      <Route path ="/Projects" element= {<Projects/>}/>
+      <Route path ="/Publications" element= {<Publications/>}/>
+      <Route path ="/Research" element= {<Research/>}/>
+      <Route path ="/Sign-up" element= {<SignUp/>}/>
+      <Route path ="/Log-in" element= {<LogIn/>}/>
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
