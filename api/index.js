face15c02a91f2820fef54feda6a9878a668e825
Your Name <you@example.com>
2024-03-24 20:11:44 +0100

    creat test api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,25 @@
 import express from'express';
 import mongoose from 'mongoose'
-import dotenv from'dotenv';{/* it for masque lien database from github */}
+import dotenv from'dotenv';
+{/* it for masque lien database from github */}
+import userRoutes from './routes/user.route.js';
+
+
+
 dotenv.config();
 {/* that mean virfy if monogo is connected if change psw make error */}
 mongoose.connect(process.env.MONGO).then(
     ()=>{
         console.log('Mongoos connected');
     }
-)
+).catch((err)=>{
+console.log(err);
+})
 const app = express();
 app.listen(3000,() =>{
     console.log('server is running on port 3000')
 
 }
-)
+);
+{/* creat api route  */}
+app.use('/api/user',userRoutes);
