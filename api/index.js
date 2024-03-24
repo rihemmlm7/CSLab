77a5d62ae6e3e0015f5bea65127e0f1e80d8f62f
Your Name <you@example.com>
2024-03-24 18:44:41 +0100

    creat user modele

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from'express';
 import mongoose from 'mongoose'
-import dotenv from'dotenv'{/* it for masque lien database from github */}
+import dotenv from'dotenv';{/* it for masque lien database from github */}
 dotenv.config();
 {/* that mean virfy if monogo is connected if change psw make error */}
 mongoose.connect(process.env.MONGO).then(
